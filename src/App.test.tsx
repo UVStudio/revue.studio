d400c969d8ca724cb265202b398e7b9e5520e801
Leonard Shen <uvstudio2199@gmail.com>
2022-07-21 16:40:00 -0400

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ButtonAppBar } from './components/AppBar';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to Revue.Studio/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders App', () => {
+  render(<App />);
+});
+
+test('renders AppBar', () => {
+  render(<ButtonAppBar />);
 });
