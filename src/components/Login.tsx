7a804f60af6796b558750bb9c43c9669cf2b70bd
Leonard Shen <uvstudio2199@gmail.com>
2022-07-26 14:03:29 -0400

    cognito token granted

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,7 +4,10 @@ import {
   CognitoUserPool,
   CognitoUserAttribute,
   CognitoUser,
+  AuthenticationDetails,
 } from 'amazon-cognito-identity-js';
+import { poolData } from '../constants/poolData';
+import * as AWS from 'aws-sdk/global';
 
 interface formData {
   name: string;
@@ -14,18 +17,23 @@ interface formData {
   phone: string;
 }
 
+const defaultFormData = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  phone: '',
+};
+
 const Login = () => {
-  const [formData, setFormData] = useState<formData>({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    phone: '',
-  });
+  //OBTAIN USER FORM INPUTS
+  const [formData, setFormData] = useState<formData>(defaultFormData);
   const [confirmCode, setConfirmCode] = useState<string>('');
+  const [register, setRegister] = useState<boolean>(true);
 
   const { name, email, password, confirmPassword, phone } = formData;
 
+  //FORM CHANGE HANDLERS
   const onChangeForm = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -38,10 +46,7 @@ const Login = () => {
     setConfirmCode(e.target.value);
   };
 
-  const poolData = {
-    UserPoolId: 'us-east-1_CXLp3WVpF', // Your user pool id here
-    ClientId: '5f609ok63mm90jlrrmc6lvh7f8', // Your client id here
-  };
+  //COGNITO USER POOL USER REGISTRATION DATA
   const userPool = new CognitoUserPool(poolData);
 
   const attributeList: CognitoUserAttribute[] = [];
@@ -61,6 +66,7 @@ const Login = () => {
   attributeList.push(attributeEmail);
   attributeList.push(attributePhoneNumber);
 
+  //COGNITO USER POOL SIGN UP
   const signUp = async () => {
     try {
       await userPool.signUp(
@@ -74,14 +80,8 @@ const Login = () => {
             return;
           }
           const cognitoUser = result!.user;
-          console.log(cognitoUser);
-          setFormData({
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-            phone: '',
-          });
+          console.log('cognito user: ', cognitoUser);
+          setFormData(defaultFormData);
         }
       );
     } catch {
@@ -89,6 +89,7 @@ const Login = () => {
     }
   };
 
+  //COGNITO USER AUTHENTICATION
   const userData = {
     Username: 'leonard.shen@gmail.com',
     Pool: userPool,
@@ -106,6 +107,58 @@ const Login = () => {
     });
   };
 
+  //COGNITO LOGIN & OBTAIN TOKEN
+  const login = async () => {
+    try {
+      const authenticationData = {
+        Username: email,
+        Password: password,
+      };
+
+      const authenticationDetails = new AuthenticationDetails(
+        authenticationData
+      );
+
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+          const accessToken = result.getAccessToken().getJwtToken();
+
+          //POTENTIAL: Region needs to be set if not already set previously elsewhere.
+          AWS.config.region = poolData.Region;
+
+          AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: poolData.IdentityPoolId, // your identity pool id here
+            Logins: {
+              // Change the key below according to the specific region your user pool is in.
+              'cognito-idp.us-east-1.amazonaws.com/us-east-1_CXLp3WVpF': result
+                .getIdToken()
+                .getJwtToken(),
+            },
+          });
+
+          //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
+          (AWS.config.credentials as AWS.CognitoIdentityCredentials).refresh(
+            (error) => {
+              if (error) {
+                console.error(error);
+              } else {
+                // Instantiate aws sdk service objects now that the credentials have been updated.
+                // example: const s3 = new AWS.S3();
+                console.log('Successfully logged! Access Token: ', accessToken);
+              }
+            }
+          );
+        },
+
+        onFailure: function (err) {
+          alert(err.message || JSON.stringify(err));
+        },
+      });
+    } catch {
+      throw new Error('Could not login user');
+    }
+  };
+
   return (
     <Box>
       <Box
@@ -117,16 +170,26 @@ const Login = () => {
         noValidate
         autoComplete="off"
       >
-        <Box className="title-container">
-          <Typography variant="h6">Register</Typography>
+        <Box>
+          <Button onClick={() => setRegister(false)}>
+            I already have an account
+          </Button>
+          /<Button onClick={() => setRegister(true)}>I am a new user</Button>
         </Box>
-        <TextField
-          required
-          id="name"
-          label="Username"
-          value={name}
-          onChange={(e) => onChangeForm(e)}
-        />
+        <Box className="title-container">
+          <Typography variant="h6">
+            {register ? 'Register' : 'Login'}
+          </Typography>
+        </Box>
+        {register ? (
+          <TextField
+            required
+            id="name"
+            label="Username"
+            value={name}
+            onChange={(e) => onChangeForm(e)}
+          />
+        ) : null}
         <TextField
           required
           id="email"
@@ -143,49 +206,62 @@ const Login = () => {
           value={password}
           onChange={(e) => onChangeForm(e)}
         />
-        <TextField
-          required
-          id="confirmPassword"
-          label="Confirm Password"
-          type="password"
-          autoComplete="confirm-password"
-          value={confirmPassword}
-          onChange={(e) => onChangeForm(e)}
-        />
-        <TextField
-          required
-          id="phone"
-          label="Phone Number"
-          value={phone}
-          onChange={(e) => onChangeForm(e)}
-        />
-        <Button variant="contained" onClick={signUp}>
-          Register
-        </Button>
+        {register ? (
+          <TextField
+            required
+            id="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            autoComplete="confirm-password"
+            value={confirmPassword}
+            onChange={(e) => onChangeForm(e)}
+          />
+        ) : null}
+        {register ? (
+          <TextField
+            required
+            id="phone"
+            label="Phone Number"
+            value={phone}
+            onChange={(e) => onChangeForm(e)}
+          />
+        ) : null}
+
+        {register ? (
+          <Button variant="contained" onClick={signUp}>
+            Register
+          </Button>
+        ) : (
+          <Button variant="contained" onClick={login}>
+            Login
+          </Button>
+        )}
       </Box>
-      <Box
-        className="section"
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <Box className="title-container">
-          <Typography variant="h6">Confirm Registration</Typography>
+      {register ? (
+        <Box
+          className="section"
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <Box className="title-container">
+            <Typography variant="h6">Confirm Registration</Typography>
+          </Box>
+          <TextField
+            required
+            id="confirmationCode"
+            label="Confirmation Code"
+            value={confirmCode}
+            onChange={(e) => onChangeCode(e)}
+          />
+          <Button variant="contained" onClick={sendConfirm}>
+            Confirm
+          </Button>
         </Box>
-        <TextField
-          required
-          id="confirmationCode"
-          label="Confirmation Code"
-          value={confirmCode}
-          onChange={(e) => onChangeCode(e)}
-        />
-        <Button variant="contained" onClick={sendConfirm}>
-          Confirm
-        </Button>
-      </Box>
+      ) : null}
     </Box>
   );
 };
---
+++ src/constants/poolData.tsx
@@ -0,0 +1,6 @@
+export const poolData = {
+  Region: 'us-east-1',
+  UserPoolId: 'us-east-1_CXLp3WVpF', // Your user pool id here
+  ClientId: '5f609ok63mm90jlrrmc6lvh7f8', // Your app client id here
+  IdentityPoolId: 'us-east-1:c98cd072-11b8-4c13-b6d4-8c43e3a21f77',
+};
