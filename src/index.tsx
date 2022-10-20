87019c95629af3ff473f267990ad9afd24ee3121
Leonard Shen <uvstudio2199@gmail.com>
2022-10-20 09:13:30 -0400

    added vitals

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import './index.css';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import reportWebVitals from '../src/reportWebVitals';
 import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -17,3 +19,5 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+reportWebVitals(console.log);
